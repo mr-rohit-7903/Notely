51f0e1c3f9a696a5a141803746d5d4100355f3de
Rohit Bej <145686961+mr-rohit-7903@users.noreply.github.com>
2025-05-17 16:15:23 +0530

    updated favicon icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Layout from './components/Layout';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello, World!</h1>
-    </div>
+    <Layout>
+      <div className="folders">
+        <p>This is where folders will be displayed.</p>
+      </div>
+    </Layout>
   );
 }
 
---
+++ src/components/Layout.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Layout({ children }) {
+    return (
+        <div className="folders">
+        </div>
+    )
+}
+
+export default Layout;
